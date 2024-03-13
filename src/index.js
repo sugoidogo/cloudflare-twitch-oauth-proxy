0aebfe19a386eb076f4a02529a43f90a20c12373
josephsmendoza <josephsmendoza@gmail.com>
2024-03-13 07:01:15 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,84 @@
+// @ts-ignore
+import token_module from '../static/TwitchAuth.mjs'
+// @ts-ignore
+import twurple_module from '../static/SugoiAuthProvider.mjs'
+// @ts-ignore
+import code_html from '../static/code.html'
+
 /**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
+ * create a Response object with control headers set
+ * @param {BodyInit} body 
+ * @param {ResponseInit} init 
  */
+function makeResponse(body=undefined,init=undefined){
+	if(!init){
+		init={}
+	}
+	if(!init.headers){
+		init.headers={}
+	}
+	init.headers['access-control-allow-origin']='*'
+	init.headers['access-control-allow-private-network']='true'
+	init.headers['cache-control']='no-cache,private'
+	return new Response(body,init)
+}
 
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+	/**
+	 * @param {Request} request 
+	 * @param {Object} env 
+	 */
+	async fetch(request, env) {
+		if(request.method==='OPTIONS'){
+			return makeResponse()
+		}
+
+		const url=new URL(request.url)
+
+		if(url.pathname==='/favicon.ico'){
+			return new Response(undefined,{status:204})
+		}
+
+		if(url.pathname==='/code.html'){
+			return makeResponse(code_html,{
+				headers:{'content-type':'text/html'}
+			})
+		}
+
+		if(url.pathname==='/TwitchAuth.mjs'){
+			// @ts-ignore
+			return makeResponse(token_module,{
+				headers:{'content-type':'text/javascript'}
+			})
+		}
+
+		if(url.pathname==='/SugoiAuthProvider.mjs'){
+			// @ts-ignore
+			return makeResponse(twurple_module,{
+				headers:{'content-type':'text/javascript'}
+			})
+		}
+
+		if(url.pathname!=='/oauth2/token'){
+			return makeResponse(undefined,{status:404});
+		}
+
+		const requestBody=await request.formData()
+
+		if(!requestBody.has('client_id')){
+			return makeResponse('missing client_id',{status:401})
+		}
+
+		const client_secret=await env.config.get(requestBody.get('client_id'))
+
+		if(client_secret===null){
+			return makeResponse(null,{status:403})
+		}
+
+		requestBody.append('client_secret',client_secret)
+		return await fetch('https://id.twitch.tv/oauth2/token',{
+			method:'POST',
+			body:requestBody
+		})
 	},
 };
---
+++ static/SugoiAuthProvider.mjs
@@ -0,0 +1,95 @@
+import TwitchAuth from "./TwitchAuth.mjs";
+
+export default class SugoiAuthProvider {
+
+    /** @type {TwitchAuth} */
+    #auth;
+
+    /** @type {String} */
+    clientId;
+
+    static #getTwurpleProxy(token){
+        return new Proxy(token,{
+            get(target, name, receiver){
+                return target[name.toString().replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`)]
+            }
+        })
+    }
+
+    constructor(client_id){
+        this.#auth=new TwitchAuth(client_id)
+        this.clientId=client_id
+    }
+
+    /**
+     * @param  {String[]} scopes 
+     * @returns {Promise<import("./TwitchAuth.mjs").TwitchToken>}
+     */
+    addUser(...scopes){
+        return this.#auth.getToken(...scopes).then(SugoiAuthProvider.#getTwurpleProxy)
+    }
+
+    removeUser(){
+        return this.#auth.resetLocalToken()
+    }
+
+    /**
+     * @param {String|Number} user 
+     * @param {String[][]} scopeSets
+     * @returns {Promise<import("./TwitchAuth.mjs").TwitchToken | null>}
+     */
+    getAccessTokenForUser(user,...scopeSets){
+        const scopes=new Set()
+        for(const scopeSet of scopeSets){
+            for(const scope of scopeSet){
+                scopes.add(scope)
+            }
+        }
+        return this.#auth.getToken(...scopes).then(SugoiAuthProvider.#getTwurpleProxy)
+        .then(token=>{
+            if(token.user_id!=user){
+                throw 'got access token for wrong user'
+            }
+            return token
+        }).catch(error=>{
+            console.warn(error)
+            return null
+        })
+    }
+
+    /**
+     * @param {String|Number} user 
+     * @returns {Promise<import("./TwitchAuth.mjs").TwitchToken>}
+     */
+    getAnyAccessToken(user){
+        return this.#auth.getToken().then(SugoiAuthProvider.#getTwurpleProxy)
+        .then(token=>{
+            if(token.user_id!=user){
+                throw 'got access token for wrong user'
+            }
+            return token.then(SugoiAuthProvider.#getTwurpleProxy)
+        }).catch(error=>{
+            return this.#auth.getAppToken().then(SugoiAuthProvider.#getTwurpleProxy)
+        })
+    }
+
+    /**
+     * @param {String|Number} user 
+     * @returns {String[]}
+     */
+    getCurrentScopesForUser(user){
+        return this.#auth.getLocalToken().scope
+    }
+
+    /**
+     * @param {String|Number} user 
+     * @returns {Promise<import("./TwitchAuth.mjs").TwitchToken>}
+     */
+    refreshAccessTokenForUser(user){
+        const token=this.#auth.getLocalToken()
+        if(token.user_id!=user){
+            throw 'got access token for wrong user'
+        }
+        return this.#auth.getFreshToken(token).then(SugoiAuthProvider.#getTwurpleProxy)
+    }
+}
